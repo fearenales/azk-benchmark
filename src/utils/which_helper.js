3667bd48d6ec802554f82ccf1e1e34c5201bcae7
Julio Makdisse Saito <saitodisse@gmail.com>
2016-02-05 14:41:47 -0200

    [Spec] Fix which test

--- src/utils/which_helper.js
+++ src/utils/which_helper.js
@@ -7,7 +7,6 @@ let whichHelper = (command_path) => {
       if (err) {
         // err is returned if no "command_path" is found on the PATH
         err = new Error(`'${command_path}' not found in PATH: ${process.env.PATH}. Original error: ${err.message}`);
-        throw err;
         reject(err);
       } else {
         // if it is found, then the absolute path to the exec is returned
